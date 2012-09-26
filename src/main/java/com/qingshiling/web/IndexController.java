194eea72e0203d6d04f16e81784454f3c5279f5c
luochuan <luochuan@novel-supertv.com>
2012-09-26 17:29:34 +0800

    Add Website title in controller.

--- src/main/java/com/qingshiling/web/IndexController.java
+++ src/main/java/com/qingshiling/web/IndexController.java
@@ -13,19 +13,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * @author rainisic
+ * Index controller. Process index request.<br>
+ * If a request mapped to "/" and no any controller processed it, the method
+ * html will process it in the final. It usually to be used in static page
+ * display.
  * 
+ * @author rainisic
+ * @version 1.0.0
+ * @since 1.0.0
  */
 @Controller
 public class IndexController {
 
+	/**
+	 * Process index request.
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("index")
 	public String index(Model model) {
+		model.addAttribute("pageTitle", "首页");
 		return "site.index.index";
 	}
 
+	/**
+	 * Process static html request.
+	 * 
+	 * @param model
+	 * @param html
+	 * @return
+	 */
 	@RequestMapping("{html}")
-	public String html(@PathVariable String html) {
+	public String html(Model model, @PathVariable String html) {
+		if (html.equals("introduction")) {
+			model.addAttribute("pageTitle", "漂流简介");
+		} else if (html.equals("direction")) {
+			model.addAttribute("pageTitle", "如何前往");
+		} else if (html.equals("about")) {
+			model.addAttribute("pageTitle", "关于我们");
+		}
 		return "site.html." + html;
 	}
 }
--- src/main/java/com/qingshiling/web/activity/ActivityController.java
+++ src/main/java/com/qingshiling/web/activity/ActivityController.java
@@ -21,45 +21,70 @@ import com.qingshiling.entity.Activity;
 import com.qingshiling.service.ActivityService;
 
 /**
- * @author lge
+ * Activity controller.
  * 
+ * @author lge
+ * @version 1.0.0
+ * @since 1.0.0
  */
 @Controller
 @RequestMapping("activity")
 public class ActivityController {
 
+	/** Define the activity service instance. */
 	@Resource
 	private ActivityService activityServiceImpl;
 
+	/**
+	 * List published activities by page.
+	 * 
+	 * @param model
+	 * @param page
+	 * @return
+	 */
 	@RequestMapping("page/{index}")
 	public String list(Model model, Page page) {
-		
+
 		// Get the activities.
 		List<Activity> activities = activityServiceImpl.list(
 				ActivityStatus.PUBLISHED, page);
-		
+
 		// Put values to request.
 		model.addAttribute("activities", activities);
+		model.addAttribute("pageTitle", "活动列表");
 		return "site.activity.list";
 	}
 
+	/**
+	 * Display an activity.
+	 * 
+	 * @param model
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping("display/{id}")
 	public String display(Model model, Integer id) {
+
+		// Get the activity.
+		Activity activity = activityServiceImpl.display(id);
+
+		// Put values to request.
+		model.addAttribute("activity", activity);
 		return "site.activity.display";
 	}
 
-	@RequestMapping
-	public String getPablishActivityList(Model model, Page page) {
-		if (page == null) {
-			throw new RuntimeException("无效参数");
-		}
-		if (page.getIndex() < 1 || page.getIndex() > 5000) {
-			throw new RuntimeException("无效参数");
-		}
-		if (page.getSize() < 1 || page.getSize() > 500) {
-			throw new RuntimeException("无效参数");
-		}
-		return null;
-	}
+	// @RequestMapping
+	// public String getPablishActivityList(Model model, Page page) {
+	// if (page == null) {
+	// throw new RuntimeException("无效参数");
+	// }
+	// if (page.getIndex() < 1 || page.getIndex() > 5000) {
+	// throw new RuntimeException("无效参数");
+	// }
+	// if (page.getSize() < 1 || page.getSize() > 500) {
+	// throw new RuntimeException("无效参数");
+	// }
+	// return null;
+	// }
 
 }
