5dbed25ae0b7a20aa6fa06ee2a8462c47ead6d0c
lihonghe <lihonghe@novel-supertv.com>
2012-09-29 10:21:40 +0800

    test sql

--- sql/activity-test-data.sql
+++ sql/activity-test-data.sql
@@ -4,3 +4,26 @@ insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) 
 insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("测试 | priority = 0 status = 2", 0, now(), 2, "测试边界");
 insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("测试 | priority = 1 status = 1", 1, now(), 1, "测试边界");
 insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("测试 | priority = 1 status = 2", 1, now(), 2, "测试边界");
+
+
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 04日，状态 = 0 ", 0, "2012/03/04 00:00:00", 0, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 04日，状态 = 0 ", 1, "2012/03/04 00:00:00", 0, "时间与优先级边界测试");
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 05日，状态 = 0 ", 0, "2012/03/05 00:00:00", 0, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 05日，状态 = 0 ", 1, "2012/03/05 00:00:00", 0, "时间与优先级边界测试");
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 04日，状态 = 1 ", 0, "2012/03/04 00:00:00", 1, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 04日，状态 = 1 ", 1, "2012/03/04 00:00:00", 1, "时间与优先级边界测试");
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 05日，状态 = 1 ", 0, "2012/03/05 00:00:00", 1, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 05日，状态 = 1 ", 1, "2012/03/05 00:00:00", 1, "时间与优先级边界测试");
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 04日，状态 = 2 ", 0, "2012/03/04 00:00:00", 2, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 04日，状态 = 2 ", 1, "2012/03/04 00:00:00", 2, "时间与优先级边界测试");
+
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 0，时间 = 05日，状态 = 2 ", 0, "2012/03/05 00:00:00", 2, "时间与优先级边界测试");
+insert into `Activity`(`content`, `priority`, `publishTime`, `status`, `title`) values("优先级 = 1，时间 = 05日，状态 = 2 ", 1, "2012/03/05 00:00:00", 2, "时间与优先级边界测试");
+
+
+
--- sql/ticket-test-data.sql
+++ sql/ticket-test-data.sql
@@ -1,1 +1,7 @@
 insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
+insert into Ticket(`type`, `price`) values("test", 120);
