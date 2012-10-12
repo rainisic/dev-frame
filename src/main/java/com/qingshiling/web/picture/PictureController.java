81242b6980f6d8cc85a4be87800bb1dc10ad1c9e
lihonghe <lihonghe@novel-supertv.com>
2012-10-12 17:38:24 +0800

    fax bug

--- src/main/java/com/qingshiling/web/picture/PictureController.java
+++ src/main/java/com/qingshiling/web/picture/PictureController.java
@@ -36,7 +36,7 @@ public class PictureController {
 	 * @param picture
 	 * @return
 	 */
-	@RequestMapping("publish")
+	@RequestMapping("admin/publish")
 	public String publish(Model model, MultipartFile pictureFile,Picture picture){
 		if(pictureFile != null && picture !=null){
 			pictureServiceImpl.publish(pictureFile, picture);
@@ -45,5 +45,9 @@ public class PictureController {
 		return "";
 	}
 
+	@RequestMapping("admin/test")
+	public String publish(Model model){
+		return "site.activity.test";
+	}
 
 }
